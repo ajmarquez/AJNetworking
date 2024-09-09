b3cc3fffa58631d6db4c2dcddcb1cc54980533d6
Abelardo Marquez <abelardo@hey.com>
2024-09-09 19:37:53 +0200

    Fixes

--- Sources/AJNetworking/APIService.swift
+++ Sources/AJNetworking/APIService.swift
@@ -3,6 +3,6 @@ import Foundation
 public protocol APIService {
     var baseURL: URL? { get }
     var encoder: JSONEncoder { get }
-    var headers: [HTTPHeaders]? { get }
+    var headers: HTTPHeaders? { get }
     var apiKey: String { get }
 }

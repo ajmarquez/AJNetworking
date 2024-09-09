b29ad88a2c1179074ead308c4e4920603c25b463
Abelardo Marquez <abelardo@hey.com>
2024-09-09 19:28:40 +0200

    Fixes

--- Sources/AJNetworking/Endpoint.swift
+++ Sources/AJNetworking/Endpoint.swift
@@ -3,6 +3,7 @@ import Foundation
 public struct Endpoint {
     let route: String
     let path: String
+    let headers: HTTPHeaders = .default()
     let method: HTTPMethod = .get
     let queryItems: [URLQueryItem]
 }

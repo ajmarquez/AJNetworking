6767e769b69428da06f10ad808c1f9a0bfd5e25a
Abelardo Marquez <abelardo@hey.com>
2024-09-11 14:31:26 +0200

    Removes path

--- Sources/AJNetworking/Endpoint.swift
+++ Sources/AJNetworking/Endpoint.swift
@@ -2,7 +2,6 @@ import Foundation
 
 public struct Endpoint {
     let route: String
-    let path: String
     let headers: HTTPHeaders = .default()
     let method: HTTPMethod = .get
     let queryItems: [URLQueryItem]
